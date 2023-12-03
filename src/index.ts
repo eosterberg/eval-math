70f208c6bfff17bfb26ff3791f5175d4da40ff58
Lumi Pakkanen <lumi.pakkanen@gmail.com>
2023-12-03 09:25:08 +0200

    Replace the evaluator with a Peggy-generated parser
    Limit the Javascript grammar to numbers only.
    Generate abstract syntax tree parser with Peggy.
    Replace existing string evaluator with an AST evaluator.
    
    ref #1

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,87 @@
-/* eslint-disable no-case-declarations */
+import {parse} from './math-ast.js';
 
-class Stack extends Array<Stack | string | number> {
-  parent?: Stack;
+type Program = {
+  type: 'Program';
+  body: ExpressionStatement[];
+};
+
+type ExpressionStatement = {
+  type: 'ExpressionStatement';
+  expression: Expression;
+};
+
+type BinaryExpression = {
+  type: 'BinaryExpression';
+  operator: '+' | '-' | '*' | '/';
+  left: Expression;
+  right: Expression;
+};
+
+type UnaryExpression = {
+  type: 'UnaryExpression';
+  operator: '-';
+  argument: Expression;
+  prefix: boolean;
+};
+
+type Literal = {
+  type: 'Literal';
+  value: number;
+};
+
+type Expression = BinaryExpression | UnaryExpression | Literal;
+
+type EvaluationContext = Map<string, number>;
+
+function parseAst(source: string): Program {
+  return parse(source);
 }
 
-const splitOnAddOrSubtract = (expressions: Stack) => {
-  if (!expressions.find(expr => expr === '+' || expr === '-')) {
-    return expressions;
+function evaluateAst(ast: Expression, context: EvaluationContext): number {
+  switch (ast.type) {
+    case 'Literal':
+      return ast.value;
   }
-
-  let currParts = new Stack();
-  const parts = new Stack();
-  parts.push(currParts);
-  for (let i = 0; i < expressions.length; i++) {
-    const expr = expressions[i];
-    if (expr === '+' || expr === '-') {
-      parts.push(expr, (currParts = []));
-    } else {
-      currParts.push(expr);
-    }
-  }
-
-  return parts;
-};
-
-const performOperation = (operation: string, left: number, right: number) => {
-  switch (operation) {
-    case '-':
-      return left - right;
-    case '*':
-      return left * right;
-    case '/':
-      return left / right;
-  }
-  return left + right;
-};
-
-const sumExpressions = (expressions: Stack) => {
-  expressions = splitOnAddOrSubtract(expressions);
-  let result = 0;
-  let currOperation;
-  for (let i = 0; i < expressions.length; i++) {
-    let expr = expressions[i];
-    if (Array.isArray(expr)) {
-      expr = sumExpressions(expr);
-    }
-
-    if (typeof expr === 'string') {
-      currOperation = expr;
-    } else {
-      result = performOperation(currOperation!, result, expr);
-    }
-  }
-
-  return result;
-};
-
-const pushNumber = (stack: Stack, numberChars: string[]) => {
-  const number = parseFloat(numberChars.join(''));
-  if (!isNaN(number)) {
-    stack.push(number);
-  }
-  return [];
-};
-
-export const evalMath = (str: string) => {
-  const strLen = str.length;
-  let i = 0;
-  let currentStack = new Stack();
-  let numberChars: string[] = [];
-
-  while (i < strLen) {
-    const char = str[i++];
-    switch (char) {
-      case '(':
-        numberChars = pushNumber(currentStack, numberChars);
-        const subStack = new Stack([]);
-        subStack.parent = currentStack;
-        currentStack.push(subStack);
-        currentStack = subStack;
-        continue;
-
-      case ')':
-        numberChars = pushNumber(currentStack, numberChars);
-        currentStack = currentStack.parent!;
-        continue;
-
-      case ' ':
-        numberChars = pushNumber(currentStack, numberChars);
-        continue;
-
+  if (ast.type === 'BinaryExpression') {
+    const left = evaluateAst(ast.left, context);
+    const right = evaluateAst(ast.right, context);
+    switch (ast.operator) {
       case '+':
+        return left + right;
       case '-':
-      case '/':
-        numberChars = pushNumber(currentStack, numberChars);
-        currentStack.push(char);
-        continue;
-
+        return left - right;
       case '*':
-      case 'x':
-      case 'X':
-        numberChars = pushNumber(currentStack, numberChars);
-        currentStack.push('*');
-        continue;
+        return left * right;
+      case '/':
+        return left / right;
+      default:
+        throw new Error(`Unimplemented operator '${(ast as any).operator}'`);
     }
-
-    if (char === ',' || char === '.') {
-      numberChars.push('.');
-    } else if (!isNaN(parseInt(char, 10))) {
-      numberChars.push(char);
+  } else if (ast.type === 'UnaryExpression') {
+    if (!ast.prefix) {
+      throw new Error('Only prefix unary operators implemented');
+    }
+    const argument = evaluateAst(ast.argument, context);
+    switch (ast.operator) {
+      case '-':
+        return -argument;
+      default:
+        throw new Error(`Unimplemented operator '${(ast as any).operator}'`);
     }
   }
+  throw new Error('Not implemented');
+}
 
-  pushNumber(currentStack, numberChars);
+export function evalMath(str: string): number {
+  const program = parseAst(str);
 
-  while (currentStack.parent) {
-    currentStack = currentStack.parent;
+  const context: EvaluationContext = new Map();
+  let result: number | undefined;
+  for (const statement of program.body) {
+    result = evaluateAst(statement.expression, context);
   }
-
-  return sumExpressions(currentStack);
-};
+  if (result === undefined) {
+    throw new Error('Expression must evaluate to a value');
+  }
+  return result;
+}
